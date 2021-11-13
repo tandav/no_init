2b22982900dfb6309331a6dcfa424a47a2fc73bf
Alexander Rodionov <alx1dr@gmail.com>
2021-11-13 15:34:16 +0300

    change setup.py, bump version

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='no_init',
-    version='0.1',
+    version='v0.1.2',
     description='tool that checks that there are no __init__.py in your modules',
     long_description_content_type="text/markdown",
     url='https://github.com/tandav/no_init',
-    packages=find_packages(),
+    # packages=find_packages(),
+    py_modules=['no_init'],
     include_package_data=True,
 )
